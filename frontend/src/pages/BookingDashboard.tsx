09d984648f9b058e7bf60cabe9e4141fc6e710f3
pawanbant-AIML <pawanbant5@gmail.com>
2026-06-05 21:53:25 +0530

    Update BookingDashboard.tsx

--- frontend/src/pages/BookingDashboard.tsx
+++ frontend/src/pages/BookingDashboard.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import axios from 'axios';
+import api from '../api';
 import BookingCard from '../components/BookingCard';
 
 interface BookingData {
